0a6d6f217084f4b4f63cacd52248f846ca3da72c
DEVENDER <devender.dabas@baeinfotech.com>
2026-09-18 11:16:13 +0530

    commit

--- backend/src/Nexora.Api/obj/Debug/net10.0/Nexora.Api.AssemblyInfo.cs
+++ backend/src/Nexora.Api/obj/Debug/net10.0/Nexora.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nexora.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb0a875586bd8ca4324fa12da73fefa4bbec620")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nexora.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nexora.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
